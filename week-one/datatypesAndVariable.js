e06c1df516c6d483599c841390b4f9cafae61c97
Jennifer-Raymondo <jenniferraymondo295@gmail.com>
2025-07-29 15:55:36 +0300

    first commit

---
+++ week-one/datatypesAndVariable.js
@@ -0,0 +1,85 @@
+/*premitive dataypes they hold single values
+
+1.// strigs/Text
+2. numbers
+
+
+intergers
+big integer
+decimals
+
+3. boolean
+true/false
+
+
+4. undefined datatype
+
+5. null(0)
+let x=null or is not applied
+
+
+
+6. symbol
+uniqness
+*/
+
+
+
+let productname ="timber"//stings we put them in quotes
+let productCost = 3500
+let transportisneeded = true
+let tablecolor
+let discount = null
+// productCost * 2
+console.log(productCost * 2)
+
+
+
+
+/*
+non primitive datatypes are lists 
+1.Array
+is a list of items
+[numbers]
+[strings]
+["sofa","table"]
+
+let productlist =["sofa" "table""cupbord]
+
+2.object 
+in js everthing is an object
+we use this curly brackets{
+name:"sofa"price:"2000"
+color:"blue"}
+
+
+3. Date
+4.Fuctions
+
+
+
+*/
+
+
+let productlist = ("sofa","table")
+
+
+let dinningtableobejct = {
+ productname2: "dining table",
+quantity: 2,
+ price: 350,
+ totalcost:quantity*price
+
+}
+
+console.log(totalcost)
+
+let paymenttype = "cash"
+const VAT = 0.05
+const PI = 3.14
+
+
+
+
+
+
---
+++ week-one/index.js
@@ -0,0 +1,32 @@
+console.log("Hello,World");
+
+let a=5
+let b=10
+let sum=a+b
+console.log(sum);
+
+console.log("i am magnifient");
+
+let y=28
+let x=15
+let sub= y-x
+console.log(sub)
+//comment
+/* multiple comment */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
