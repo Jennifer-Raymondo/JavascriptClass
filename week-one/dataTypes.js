6e54d8aa6313e2716dc421213a4e11b641abb600
Jennifer-Raymondo <jenniferraymondo295@gmail.com>
2025-08-22 18:37:26 +0300

    my pushed work for week one

--- week-one/dataTypes.js
+++ week-one/dataTypes.js
@@ -9,7 +9,7 @@ console.log(typeof (nameOf));      // string
 console.log(typeof (isWorking));   // boolean
 console.log(typeof (weight));      // undefined
 console.log(typeof (color));       // object
-
+//.
 
 
 
